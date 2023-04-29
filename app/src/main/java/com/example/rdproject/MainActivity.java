e02db4ac5f970231a7e3083fd37020bdeb427d1e
Alexandru <alex03aioanei@gmail.com>
2023-04-30 02:40:54 +0300

    commit - bottom nav - I have created the fragments that we are going to use in order to see our screens the proper way.

--- app/src/main/java/com/example/rdproject/MainActivity.java
+++ app/src/main/java/com/example/rdproject/MainActivity.java
@@ -1,30 +1,75 @@
 package com.example.rdproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
+
+import nl.joery.animatedbottombar.AnimatedBottomBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    AnimatedBottomBar animatedMenu;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureButton();
+        animatedMenu = findViewById(R.id.animatedBottomBar);
 
-    }
-
-    private void configureButton(){
-        Button button = (Button) findViewById(R.id.buttonlogin);
-        button.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View view){
-            startActivity(new Intent(MainActivity.this, LogIn.class));
+        if(savedInstanceState==null)
+        {
+            animatedMenu.selectTabById(R.id.home1, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragmentContainer, homeFragment).commit();
         }
+
+        animatedMenu.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
+            @Override
+            public void onTabSelected(int lastIndex, @Nullable AnimatedBottomBar.Tab lastTab, int newIndex, @NonNull AnimatedBottomBar.Tab newTab) {
+                Fragment fragment = null;
+
+                if(newTab.getId() == R.id.home1)
+                {
+                    fragment = new HomeFragment();
+                }
+                else if(newTab.getId() == R.id.popular1)
+                {
+                    fragment = new PopularFragment();
+                }
+                else if(newTab.getId() == R.id.account1)
+                {
+                    fragment = new AccountFragment();
+                }
+
+                if(fragment!=null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
+                }
+                else{
+                    System.out.println("error in casting fragment");
+                }
+            }
+
+            @Override
+            public void onTabReselected(int i, @NonNull AnimatedBottomBar.Tab tab) {
+
+            }
+
         });
     }
+
+
 }
