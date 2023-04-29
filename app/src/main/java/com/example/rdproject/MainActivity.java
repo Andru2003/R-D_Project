99d47f300639059ab1a316025574a52045918aa4
Alexandru <alex03aioanei@gmail.com>
2023-04-29 20:57:08 +0300

    commit - bottom nav - 2 Just some debugging

--- app/src/main/java/com/example/rdproject/MainActivity.java
+++ app/src/main/java/com/example/rdproject/MainActivity.java
@@ -4,38 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.rdproject.R.id;
-
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Daca vedeti asta inseamna ca e bine");
-        System.out.println("Sanatate si numai bine!");
-        System.out.println("ce faci?");
-        System.out.println("Ascultam ploaia!");
-        System.out.println("dar nu ploaua");
-        System.out.println("Nu ploua? Nu cred");
-        System.out.println("Nu bagati de seama culoarea la meniu trebuie facut un collor pallete pt aplicatie");
-        System.out.println("Stefan si a pierdut cheile de la casa io mor.");
+        configureButton();
 
-        btn = findViewById(R.id.main_button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent( MainActivity.this, LogIn.class);
-                startActivity(intent);
-                finish();
-            }
+    }
+
+    private void configureButton(){
+        Button button = (Button) findViewById(R.id.buttonlogin);
+        button.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            startActivity(new Intent(MainActivity.this, LogIn.class));
+        }
         });
-
     }
 }
